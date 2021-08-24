9cab2f968300aa8dc3b1f08b3dfd0754de93d296
Babay <senjuid@gmail.com>
2021-08-24 11:56:15 +0700

    Update BlurPrivacyScreen.m

--- src/ios/BlurPrivacyScreen.m
+++ src/ios/BlurPrivacyScreen.m
@@ -7,36 +7,11 @@
 @implementation AppDelegate (BlurAppPrivacyScreen)
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    self.window.backgroundColor = [UIColor clearColor];
-
-    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight];
-    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-    blurEffectView.frame = self.window.bounds;
-    blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-
-    blurEffectView.tag = 1234;
-    blurEffectView.alpha = 0;
-    [self.window addSubview:blurEffectView];
-    [self.window bringSubviewToFront:blurEffectView];
-
-    // fade in the view
-    [UIView animateWithDuration:0.5 animations:^{
-        blurEffectView.alpha = 1;
-    }];
+    self.window.isHidden = true;
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    NSLog(@"Hiding blur");
-    // grab a reference to our custom blur view
-    UIView *blurEffectView = [self.window viewWithTag:1234];
-
-    // fade away colour view from main view
-    [UIView animateWithDuration:0.5 animations:^{
-        blurEffectView.alpha = 0;
-    } completion:^(BOOL finished) {
-        // remove when finished fading
-        [blurEffectView removeFromSuperview];
-    }];
+    self.window.isHidden = false;
 }
 
 @end
